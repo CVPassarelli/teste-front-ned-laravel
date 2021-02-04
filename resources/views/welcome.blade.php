e501431b600799ae78aa52213885e6dd35280c2d
Caio Passarelli <cvpassarelli@gmail.com>
2021-02-04 12:08:57 -0300

    initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Caio Vinicius Bergo Passarelli</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+        <style>
+            header{
+                width: 100%;
+                border: 1px solid #000;
+                display: flex;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <button type="button"></button>
+        </header>
+
+
+    <script src="{{ asset('js/services.js') }}"></script>
+    </body>
+</html>
